fb74cb2a15beeb5fb1542ec38c572bee3a35e684
Cody Boyko <codyboyko@yahoo.ca>
2014-06-11 16:57:03 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp', []);
+
+/*app.run(function($rootScope) {
+	$rootScope.name = "Cody Boyko";
+});*/
+
+app.controller('ParentController', function($scope) {
+	$scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
+	$scope.sayHello = function() {
+		$scope.person.greeted = true;
+	};
+});
+
+/*app.controller('MyController', function($scope) {
+	$scope.person = {
+		name: "Cody Boyko"
+	};
+});*/
